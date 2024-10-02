d5d3b9826af7592c1974a3dce62040466f1dbab2
alex <118021985+alxcd@users.noreply.github.com>
2024-10-02 17:10:48 +0300

    added proper api response to client

--- public/main.js
+++ public/main.js
@@ -1,14 +1,16 @@
-function searchPerson(id) {
-  const person = document.getElementById(id);
+function searchPerson(input_id) {
+  const person = document.getElementById(input_id);
   console.log(person.value);
-  searchApi(person.value, "person");
+  searchApi(person.value, "person")
+    .then(data => {console.log(data)});
 }
 
-
 function searchApi(query, type) {
-  fetch(`/api/search?query=${query}&type=${type}`)
+  return fetch(`/api/search?query=${query}&type=${type}`)
     .then(response => response.json())
-    .then(response => console.log(response))
     .catch(err => console.error(err));  
 }
 
+function getImage() {
+
+}
--- server.js
+++ server.js
@@ -7,21 +7,32 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+};
+
+const configuration = fetch('https://api.themoviedb.org/3/configuration', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+
+console.log(configuration);
+
 app.get('/api/search', (req, res) => {
   const {query, type} = req.query;
   const url = `https://api.themoviedb.org/3/search/${type}?query=${encodeURIComponent(query)}`;
 
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${API_KEY}`
-    }
-  };
-
   fetch(url, options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => {
+      console.log(response);
+      res.json(response);
+    })
     .catch(err => console.error(err));  
 })
 
